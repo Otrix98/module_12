f035bc899a4ced0ce42f8d5dda17e0bf45bb4d59
Otrix98 <otrix98@gmail.com>
2020-07-03 20:53:03 +0300

    module 12.2

--- app/src/main/java/com/example/module_12/ListFragment.kt
+++ app/src/main/java/com/example/module_12/ListFragment.kt
@@ -2,8 +2,10 @@ package com.example.module_12
 
 import android.os.Bundle
 import android.view.Gravity
+import android.view.View
 import android.widget.LinearLayout
 import android.widget.ProgressBar
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.LifecycleOwner
@@ -17,15 +19,60 @@ import kotlin.reflect.KProperty
 class ListFragment: Fragment (R.layout.fragment_list) {
 
 
-     var currentWeight: String = "0"
 
     fun editWeight (weight: String) {
         currentWeight = weight
+        foodUpdate()
         addFood()
+//        Toast.makeText(context, "current weight = " + currentWeight, Toast.LENGTH_LONG).show()
+
 
     }
 
-    private var foods = listOf(
+    private var currentWeight: String = "0"
+
+
+    private var currentFood: List<Food> = emptyList()
+
+    fun foodUpdate(){
+        foods = listOf(
+            Food.Dish (
+                name = "Карбонара",
+                weight = currentWeight,
+                avatarLink = "https://domos.ru/images/blog/15944/Carbonara.jpg",
+                structure = "Спагетти, бекон, яйцо, сливки"
+            ),
+            Food.Dish (
+                name = "Винегрет́",
+                weight = currentWeight,
+                avatarLink = "https://avatars.mds.yandex.net/get-pdb/245485/10b9d137-9069-4ebc-a993-82a6076689f0/s1200?webp=false",
+                structure = "свёкла, картофель, фасоль, морковь, огурцы"
+            ),
+            Food.Dish (
+                name = "Ризотто",
+                weight = currentWeight,
+                avatarLink = "https://avatars.mds.yandex.net/get-pdb/1757027/4aa3f258-d4d7-4f0f-815d-a2aaa912c062/s1200?webp=false",
+                structure = "Рис, масло, лук, чеснок, креветки"
+            ),
+            Food.Product (
+                name = "Рыба",
+                weight = currentWeight,
+                avatarLink = "https://avatars.mds.yandex.net/get-pdb/1535406/32215f6e-737d-4229-8a4f-60c553984775/s1200?webp=false"
+            ),
+            Food.Product (
+                name = "Рис",
+                weight = currentWeight,
+                avatarLink = "https://attuale.ru/wp-content/uploads/2019/01/Sollana-paella-rice-new-website.jpg"
+            ),
+            Food.Product (
+                name = "Курица",
+                weight = currentWeight,
+                avatarLink = "https://i.baraholka.com.ru/files/1/8/1819452.jpg"
+            )
+        )
+    }
+
+    var foods = listOf(
         Food.Dish (
             name = "Карбонара",
             weight = currentWeight,
@@ -61,17 +108,17 @@ class ListFragment: Fragment (R.layout.fragment_list) {
         )
     )
 
-    private var currentFood: List<Food> = emptyList()
-
 
     private var foodAdapter: FoodAdapter? = null
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        isTextVisiable()
         initList()
         addFab.setOnClickListener { WeightDialogFragment().show(childFragmentManager, "Tag") }
         foodAdapter?.updateFoods(currentFood)
         foodAdapter?.notifyDataSetChanged()
+
     }
 
     override fun onDestroyView() {
@@ -85,7 +132,16 @@ class ListFragment: Fragment (R.layout.fragment_list) {
             ?.let { it as? WeightDialogFragment }
             ?.dismiss()
     }
-    
+
+    fun isTextVisiable(){
+        if (currentFood.isEmpty()) {
+            textEmpty.visibility = View.VISIBLE
+        } else if (currentFood.isNotEmpty()){
+            textEmpty.visibility = View.GONE
+        }
+    }
+
+
 
 //
 //    private fun textEmpty() {
@@ -117,18 +173,24 @@ class ListFragment: Fragment (R.layout.fragment_list) {
 
 
     private fun deleteFood( position: Int) {
+
         currentFood = currentFood.filterIndexed { index, food -> index != position }
         foodAdapter?.updateFoods(currentFood)
         foodAdapter?.notifyItemRemoved(position)
+        isTextVisiable()
     }
 
     private fun addFood() {
+
         val newFood = foods.random()
         currentFood = listOf(newFood) + currentFood
         foodAdapter?.updateFoods(currentFood)
         foodAdapter?.notifyItemInserted(0 )
+        isTextVisiable()
         foodList.scrollToPosition(0)
     }
+
+
 
 
 
--- app/src/main/java/com/example/module_12/WeightDialogFragment.kt
+++ app/src/main/java/com/example/module_12/WeightDialogFragment.kt
@@ -2,29 +2,58 @@ package com.example.module_12
 
 import android.app.AlertDialog
 import android.app.Dialog
+import android.content.DialogInterface
 import android.os.Build
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.DialogFragment
+import kotlinx.android.synthetic.main.fragment_list.*
 import kotlinx.android.synthetic.main.weight_fragment.*
+import kotlinx.android.synthetic.main.weight_fragment.view.*
 
 
 class WeightDialogFragment: DialogFragment() {
-    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
-    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        return  AlertDialog.Builder(requireActivity())
-            .setView(R.layout.weight_fragment)
-            .setTitle("Введите вес")
-            .setPositiveButton("Применить", {_, _ ->
-                showElement(editText.text.toString())
-                        Log.d("weight","${editText.text.toString()}")})
-            .setNegativeButton("Отмена", {_, _ -> })
-            .create()
+    private lateinit var dialogView: View
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+        dialogView = activity!!.layoutInflater.inflate(R.layout.weight_fragment, null)
+//        dialogView = inflater.inflate(R.layout.weight_fragment, container)
+        return dialogView
     }
 
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        dialogView = activity!!.layoutInflater.inflate(R.layout.weight_fragment, null)
+        val editText  = dialogView.findViewById<EditText>(R.id.editText)
+        return AlertDialog.Builder(requireContext())
+            .setView(dialogView)
+            .setTitle("Введите вес")
+            .setPositiveButton("Применить", object : DialogInterface.OnClickListener {
+                override fun onClick(p0: DialogInterface?, p1: Int) {
+//                    Toast.makeText(context, "EditText is " + editText.text.toString(), Toast.LENGTH_SHORT).show()
+                    showElement(editText.text.toString())
+                    p0?.dismiss()
+                }
+            })
+            .setNegativeButton("Отмена", object : DialogInterface.OnClickListener {
+                override fun onClick(p0: DialogInterface?, p1: Int) {
+                    p0?.dismiss()
+                }
+            })
+            .create()
+    }
 
 
     fun showElement(weight: String){
